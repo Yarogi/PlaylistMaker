dc42d78733f09b2bd06b81b441c1f4a688fc3252
Yarogi <yachmenyov.igor@yandex.ru>
2024-04-15 15:30:14 +0300

    Изменены отступы в соответсвии в сторогом соответсвии с шалоном. На экранах заглушках заголовки формы приведены к общему стандарту. Убраны комментарии.

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,5 @@
 package com.example.playlistmaker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,11 +11,6 @@ class SettingsActivity : AppCompatActivity() {
 
         val exitButton = findViewById<Button>(R.id.exitBtn)
         exitButton.setOnClickListener{
-            // //Переход на главный экран я закомменировал, так как забивает стек,
-            // // реализовал просто через закрытие
-            //val mainIntent = Intent(this, MainActivity::class.java)
-            //startActivity(mainIntent)
-            //
             this.finish()
         }
 
