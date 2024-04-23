e72b7bf1778a6e35467bbea645d3fb3d58de61cc
Yarogi <yachmenyov.igor@yandex.ru>
2024-04-23 11:19:22 +0300

    RecyclerView подключен

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -79,7 +79,7 @@ class SearchActivity : AppCompatActivity() {
                 "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
             )
         )
-
+        trackListView.adapter = TrackAdapter(trackListContent)
 
     }
 
