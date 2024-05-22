c6f8bb28d1d11457e87d46ec5740b9a7d9661567
Yarogi <yachmenyov.igor@yandex.ru>
2024-05-22 16:45:45 +0300

    13. Создание активити плеера (дополнить нижнбб часть)

---
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -0,0 +1,14 @@
+package com.example.playlistmaker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_player)
+
+    }
+}
