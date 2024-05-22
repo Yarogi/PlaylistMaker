130cbc814e6299e3881453f3b9b1e1c56fa0ca85
Yarogi <yachmenyov.igor@yandex.ru>
2024-05-22 14:21:47 +0300

    13. Добавлено полученнии url обложки

--- app/src/main/java/com/example/playlistmaker/model/Track.kt
+++ app/src/main/java/com/example/playlistmaker/model/Track.kt
@@ -1,13 +1,11 @@
 package com.example.playlistmaker.model
 
-import com.google.gson.annotations.SerializedName
-import java.text.SimpleDateFormat
-import java.util.Locale
-
 data class Track(
-    val trackId:Int,
+    val trackId: Int,
     val trackName: String,
     val artistName: String,
     val trackTimeMillis: Float,
     val artworkUrl100: String
-)
+) {
+    fun getCoverArtwork() = artworkUrl100.replaceAfterLast("/", "512x512bb.jpg")
+}
