bd09a181ff798a5af1caac067b0837b09b7e977c
Yarogi <yachmenyov.igor@yandex.ru>
2024-06-13 16:23:25 +0300

    Прогресс бар и debounce

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -3,6 +3,8 @@ package com.example.playlistmaker
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
@@ -26,6 +28,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
+    private var trackSelectionIsProcessed = false
+
     private var savedSearchText: String = SEARCH_DEF
     private val trackList = ArrayList<Track>()
 
@@ -40,6 +44,7 @@ class SearchActivity : AppCompatActivity() {
     //Global-Views
     private lateinit var errorHolderEmpty: View
     private lateinit var errorHolderNoConnection: View
+    private lateinit var searchTextEdit: EditText
 
     //TrackList
     private lateinit var trackListView: RecyclerView
@@ -56,16 +61,26 @@ class SearchActivity : AppCompatActivity() {
     private val history = ArrayList<Track>()
     private val historyClickListener = object : TrackAdapter.Listener {
         override fun onClickTrackListener(track: Track) {
+
+            if (trackSelectionIsProcessed) return
+            trackSelectionIsProcessed = true
+
             //replace track in history
             replaceTrackInHistory(track)
             startingTrack(track)
+
+            trackSelectionIsProcessed = false
+
         }
     }
     private val historyAdapter = TrackAdapter(historyClickListener)
     private lateinit var historyHolder: LinearLayout
     private lateinit var historyListView: RecyclerView
 
+    //Search sync
     private lateinit var progressBar: ProgressBar
+    private val searchRunnable = Runnable { searchTrack() }
+    private val mainHandler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -73,8 +88,8 @@ class SearchActivity : AppCompatActivity() {
 
         //Initializing views
         val exitButton = findViewById<Button>(R.id.exitBtn)
-        val searchTextEdit = getSearchTextEditView()
         val clearSearchText = findViewById<ImageButton>(R.id.clearSearchText)
+        searchTextEdit = findViewById(R.id.searchTextEdit)
         progressBar = findViewById(R.id.progressBar)
 
         //Exit
@@ -84,7 +99,7 @@ class SearchActivity : AppCompatActivity() {
 
         //Clear text
         clearSearchText.setOnClickListener {
-            setTextInSearchEdit(SEARCH_DEF, searchTextEdit)
+            setTextInSearchEdit(SEARCH_DEF)
             hideKeyboard()
 
             trackList.clear()
@@ -110,6 +125,9 @@ class SearchActivity : AppCompatActivity() {
                 val visibility =
                     if (searchTextEdit.hasFocus() && s?.isEmpty() == true) View.VISIBLE else View.GONE
                 updateHistoryVisible(visibility)
+
+                //Search track
+                searchTrackDebounce()
 
             }
 
@@ -156,17 +174,12 @@ class SearchActivity : AppCompatActivity() {
         return if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
     }
 
-    private fun getSearchTextEditView(): EditText {
-        return findViewById(R.id.searchTextEdit)
-    }
-
     private fun getTrackListView(): RecyclerView {
         return findViewById(R.id.trackListView)
     }
 
-    private fun setTextInSearchEdit(text: String, searchTextEdit: EditText? = null) {
-        val textEdit = searchTextEdit ?: getSearchTextEditView()
-        textEdit.setText(text)
+    private fun setTextInSearchEdit(text: String) {
+        searchTextEdit.setText(text)
     }
 
     private fun hideKeyboard() {
@@ -181,7 +194,8 @@ class SearchActivity : AppCompatActivity() {
     private fun updateVisibiltyViews(
         noConnection: Boolean = false,
         empty: Boolean = false,
-        hideList: Boolean = false
+        hideList: Boolean = false,
+        showProgressBar: Boolean = false
     ) {
 
         errorHolderNoConnection.visibility = View.GONE
@@ -192,6 +206,7 @@ class SearchActivity : AppCompatActivity() {
         when {
             noConnection -> errorHolderNoConnection.visibility = View.VISIBLE
             empty -> errorHolderEmpty.visibility = View.VISIBLE
+            showProgressBar -> progressBar.visibility = View.VISIBLE
             !hideList -> trackListView.visibility = View.VISIBLE
         }
 
@@ -229,7 +244,7 @@ class SearchActivity : AppCompatActivity() {
         readSavedHistory()
         historyAdapter.tracks = history
 
-        val visibility = if (getSearchTextEditView().hasFocus()) View.VISIBLE else View.GONE
+        val visibility = if (searchTextEdit.hasFocus()) View.VISIBLE else View.GONE
         updateHistoryVisible(visibility)
 
         val clearHistoryBtn = findViewById<Button>(R.id.clearHistory)
@@ -258,14 +273,32 @@ class SearchActivity : AppCompatActivity() {
 
     }
 
+    private fun searchTrackDebounce() {
+
+        mainHandler.removeCallbacks(searchRunnable)
+
+        if (searchTextEdit.text.isNotEmpty()) {
+            mainHandler.postDelayed(searchRunnable, SEARCH_DELAY)
+        } else {
+            progressBar.visibility = View.GONE
+        }
+
+    }
+
     private fun searchTrack() {
 
-        progressBar.visibility = View.VISIBLE
+        updateVisibiltyViews(
+            showProgressBar = true
+        )
 
         trackSearchService.search(savedSearchText).enqueue(object : Callback<TrackResponse> {
             override fun onResponse(
                 call: Call<TrackResponse>, response: Response<TrackResponse>
             ) {
+
+                if (searchTextEdit.text.isEmpty()) {
+                    return
+                }
 
                 if (response.code() == 200) {
 
@@ -365,6 +398,9 @@ class SearchActivity : AppCompatActivity() {
         //search preferences
         const val SEARCH_PREFERENCES = "playlistmaker_search_preferences"
         const val HISTORY_KEY = "search_history"
+
+        //search debounce
+        private const val SEARCH_DELAY = 2000L
     }
 
 }
