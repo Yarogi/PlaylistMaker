8db20b2878fb556f83b7058f1533df4483530b1d
Yarogi <yachmenyov.igor@yandex.ru>
2024-07-17 12:04:06 +0300

    Спринт 16. Подготовка Settings без реализации

--- app/src/main/java/com/example/playlistmaker/domain/settings/impl/SettingsInteractorImpl.kt
+++ app/src/main/java/com/example/playlistmaker/domain/settings/impl/SettingsInteractorImpl.kt
@@ -1,6 +1,14 @@
 package com.example.playlistmaker.domain.settings.impl
 
 import com.example.playlistmaker.domain.settings.SettingsInteractor
+import com.example.playlistmaker.domain.settings.model.ThemeSettings
 
 class SettingsInteractorImpl:SettingsInteractor {
+    override fun getThemeSettings(): ThemeSettings {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateThemeSettings(settings: ThemeSettings) {
+        TODO("Not yet implemented")
+    }
 }
