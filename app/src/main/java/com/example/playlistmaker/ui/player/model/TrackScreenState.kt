566518611374aef67811b511ccaeffeb5d8638b6
Yarogi <yachmenyov.igor@yandex.ru>
2024-07-25 10:30:03 +0300

    4. data-object

--- app/src/main/java/com/example/playlistmaker/ui/player/model/TrackScreenState.kt
+++ app/src/main/java/com/example/playlistmaker/ui/player/model/TrackScreenState.kt
@@ -3,6 +3,6 @@ package com.example.playlistmaker.ui.player.model
 import com.example.playlistmaker.domain.main.model.Track
 
 sealed class TrackScreenState {
-    object Loading : TrackScreenState()
+    data object Loading : TrackScreenState()
     data class Content(val track: Track) : TrackScreenState()
 }
