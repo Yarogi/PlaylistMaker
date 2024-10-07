51b8b7a5152b2d92fec520f1beb2330fb2d0fee3
Yarogi <yachmenyov.igor@yandex.ru>
2024-10-07 13:00:19 +0300

    Поля ввода

--- app/src/main/java/com/example/playlistmaker/presentation/media_library/playlists/PlaylistViewModel.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/media_library/playlists/PlaylistViewModel.kt
@@ -1,8 +1,6 @@
 package com.example.playlistmaker.presentation.media_library.playlists
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.navigation.fragment.NavHostFragment
 
 class PlaylistViewModel : ViewModel() {
 
