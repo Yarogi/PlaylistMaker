e16bf502dd66f4cc6c521d4fad9bc0a720be7fe0
Yarogi <yachmenyov.igor@yandex.ru>
2024-10-07 10:42:43 +0300

    Правки по ревью 1

--- app/src/main/java/com/example/playlistmaker/ui/RootActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/RootActivity.kt
@@ -23,9 +23,6 @@ class RootActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
 
         binding.bottomNavigationView.setupWithNavController(navController)
-//        navController.addOnDestinationChangedListener { _, destination, _ ->
-//            TODO()
-//        }
 
     }
 
