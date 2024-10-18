37d2d2107695c538bcb8eb4b6d38c744dab070c5
IGOR_MSI <yachmenyov.igor@yandex.ru>
2024-10-18 21:55:14 +0300

    Исправление бага

--- app/src/main/java/com/example/playlistmaker/ui/playlists/list/PlaylistViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/ui/playlists/list/PlaylistViewHolder.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.domain.playlists.model.Playlist
@@ -25,6 +26,8 @@ class PlaylistViewHolder(view: View) : ViewHolder(view) {
 
         Glide.with(itemView)
             .load(playlist.coverPathUri)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .skipMemoryCache(true)
             .placeholder(R.drawable.track_placeholder)
             .transform(CenterCrop(), RoundedCorners(pxToDP(itemView.context, 8)))
             .into(coverView)
